5afb7461b81e7f008320bc5756e529f7d0156618
ShiroNhL <shironhl2077@gmail.com>
2023-01-04 02:57:34 +0100

    'v0.1***'

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,16 +1,39 @@
-export default function Navbar({members}) {
-    return (
-      <nav>
-        <ul className="nav">
-          {members.map((member) => {
-            const { id, username, image} = member;
-              return <li key={id} className='nav-item'>
-                  <img src={image} alt="!!!" className="icon"></img>
-                  {username}
-              </li>;
-          })}
-            </ul>
+import { useState } from "react";
 
-      </nav>
-    );
+export default function Navbar({ members }) {
+  // function toggleHidden() {
+  //   const usr = document.getElementsByClassName("usr")
+  //   usr.classList.toggle("hidden");
+  // }
+
+  const [isHovering, setIsHovering] = useState(false);
+
+   const handleMouseOver = () => {
+     setIsHovering(true);
+   };
+
+   const handleMouseOut = () => {
+     setIsHovering(false);
+   };
+
+  return (
+    <nav>
+      <ul className="nav">
+        {members.map((member) => {
+          const { id, username, image } = member;
+          return (
+            <li
+              key={id}
+              className="nav-item"
+              onMouseOver={handleMouseOver}
+              onMouseOut={handleMouseOut}
+            >
+              <img src={image} alt="!!!" className="icon"></img>
+              <span className={isHovering ? "" : "hidden"}>{username}</span>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
 }
